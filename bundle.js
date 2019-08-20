e4d01bc37bb35440a5869e7ee8af7c2c60028ca9
Denis Novikov <den-vse@yandex.ru>
2019-08-20 21:31:56 +0300

    uncomment main code

--- bundle.js
+++ bundle.js
@@ -1,22 +1,20 @@
 window.onload = function () {
-  alert('onload');
-  /*
   let x = null;
-    const accelerationHandler = (e) => {
-      const { acceleration } = e;
+  const accelerationHandler = (e) => {
+    const { acceleration } = e;
 
-      if (acceleration.x !== null) {
-        x = acceleration.x;
-      }
+    if (acceleration.x !== null) {
+      x = acceleration.x;
+    }
 
-      window.removeEventListener('devicemotion', accelerationHandler);
-    };
+    window.removeEventListener('devicemotion', accelerationHandler);
+  };
 
-    window.addEventListener('devicemotion', accelerationHandler);
+  window.addEventListener('devicemotion', accelerationHandler);
 
-    setTimeout(() => {
-      if (x === null) {
-        this.setState({ noAcceleration: true });
-      }
-    }, 1500);*/
+  setTimeout(() => {
+    if (x === null) {
+      alert('noAcceleration');
+    }
+  }, 1500);
 };
